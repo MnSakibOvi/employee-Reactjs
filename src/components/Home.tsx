cec0a1b6333dbb95cfa86e33093bdad08744dc66
MnSakibOvi <85490358+MnSakibOvi@users.noreply.github.com>
2024-01-31 01:33:19 +0600

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,9 +4,9 @@ import "./Home.css";
 function Home() {
     return (
         <div className="home-container">
-            <h3 className="home-heading">Welcome to 6sense Technologies</h3>
+            <h3 className="home-heading">Welcome</h3>
             <p className="home-content">
-            At 6senseEV, we specialize in providing a pioneering EV charging management software, empowering businesses to efficiently operate and optimize their charging infrastructure. With our expertise and innovative solutions, we are driving the future of sustainable transportation.
+            we specialize in providing a pioneering EV charging management software, empowering businesses to efficiently operate and optimize their charging infrastructure. With our expertise and innovative solutions, we are driving the future of sustainable transportation.
             </p>
         </div>
     );
